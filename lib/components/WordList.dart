f60cc39030a45ecc19ea69efc03255185855a949
Anastasiya Nesteryukova <anastasiya.nesteryukova.adci@gmail.com>
2020-08-14 12:32:16 +0600

    Initial commit

---
+++ lib/components/WordList.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class WordList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    ListView(
+      children: <Widget>[
+        ListTile(
+          leading: Icon(Icons.map),
+          title: Text('Map'),
+        ),
+        ListTile(
+          leading: Icon(Icons.photo_album),
+          title: Text('Album'),
+        ),
+        ListTile(
+          leading: Icon(Icons.phone),
+          title: Text('Phone'),
+        ),
+      ],
+    );
+  }
+}
