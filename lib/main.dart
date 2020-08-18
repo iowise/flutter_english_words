834dd979e12f97c9f717386d63561e8f015dfd5f
EvgeneOskin <eugene@termius.com>
2020-08-18 22:45:34 +1200

    Add montserrat font

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/pages/Words.dart';
-import 'package:flutter_app/pages/WordDetails.dart';
+import 'package:google_fonts/google_fonts.dart';
+import './pages/Words.dart';
+import './pages/WordDetails.dart';
+import './pages/WordTrain.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,12 +16,20 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.amber,
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        textTheme: GoogleFonts.montserratTextTheme(
+          Theme.of(context).textTheme.copyWith(
+                headline1: TextStyle(fontSize: 72.0),
+                headline4: TextStyle(fontSize: 36.0),
+                bodyText1: TextStyle(fontSize: 20.0),
+              ),
+        ),
       ),
-      routes: <String, WidgetBuilder> {
-        '/': (BuildContext context) => WordDetails(title: 'Enter word'),
-        '/train': (BuildContext context) => WordsPage(title: 'page C'),
+      routes: <String, WidgetBuilder>{
+        '/': (BuildContext context) => TrainPage(title: 'Train a word'),
+        '/train': (BuildContext context) => TrainPage(title: 'Train a word'),
         '/words': (BuildContext context) => WordsPage(title: 'Words'),
-        '/word/create': (BuildContext context) => WordDetails(title: 'Enter word'),
+        '/word/create': (BuildContext context) =>
+            WordDetails(title: 'Enter word'),
       },
     );
   }
