0aba63fbe9495de5fd77db6022edc9410bd315a8
EvgeneOskin <eugene@termius.com>
2020-08-18 22:46:02 +1200

    Add training page

---
+++ lib/components/Train.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/components/WordList.dart';
+
+class Train extends StatefulWidget {
+  final WordEntry entry;
+  final bool isCheck;
+  final Function onSubmit;
+
+  Train({Key key, this.entry, this.isCheck, this.onSubmit}) : super(key: key);
+
+  @override
+  _TrainState createState() => _TrainState();
+}
+
+class _TrainState extends State<Train> {
+  final _formKey = GlobalKey<FormState>();
+
+  String enteredWord;
+
+  @override
+  Widget build(BuildContext context) {
+    final results = widget.isCheck
+        ? [
+            Flexible(
+                flex: 1,
+                fit: FlexFit.tight,
+                child: _TrainResult(
+                    enteredWord: enteredWord, word: widget.entry.word))
+          ]
+        : [];
+    return Form(
+      key: _formKey,
+      child: Scrollbar(
+        child: Align(
+          alignment: Alignment.topCenter,
+          child: Column(
+            children: <Widget>[
+              Flexible(
+                flex: 2,
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(32.0),
+                    child: Text(widget.entry.translation,
+                        style: Theme.of(context).textTheme.bodyText1),
+                  ),
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: TextFormField(
+                    enabled: !widget.isCheck,
+                    style: Theme.of(context).textTheme.headline4,
+                    decoration: InputDecoration(
+                      filled: true,
+                      hintText: widget.isCheck ? "" : 'Enter a word...',
+                    ),
+                    onFieldSubmitted: (_) {
+                      widget.onSubmit();
+                    },
+                    onChanged: (value) {
+                      enteredWord = value;
+                    },
+                  ),
+                ),
+              ),
+              ...results,
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _TrainResult extends StatelessWidget {
+  final String word;
+  final String enteredWord;
+
+  const _TrainResult({Key key, this.word, this.enteredWord}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final correct = word == enteredWord;
+    return Card(
+      color: correct ? Colors.green : Colors.red,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(children: [
+            Flexible(
+                flex: 1,
+                child: Text(correct ? "Correct" : "Wrong",
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyText1
+                        .copyWith(color: Colors.white))),
+            Flexible(
+                flex: 1,
+                child: Text(word,
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline4
+                        .copyWith(color: Colors.white))),
+          ]),
+        ),
+      ),
+    );
+  }
+}
