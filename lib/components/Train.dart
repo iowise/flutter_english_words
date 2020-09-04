b40ae10894fbe786f1fea8fdc4a9b4bd993c4a83
EvgeneOskin <eugene@termius.com>
2020-09-04 22:42:50 +1200

    Fix rendering multiline text in train

--- lib/components/Train.dart
+++ lib/components/Train.dart
@@ -5,10 +5,10 @@ import '../models/WordEntryRepository.dart';
 typedef ResultCallback = void Function();
 
 class TrainController extends TextEditingController {
-
   final WordEntry entry;
 
   TrainController(this.entry);
+
   bool get isCorrect => entry.word == text;
 }
 
@@ -18,7 +18,13 @@ class Train extends StatefulWidget {
   final ResultCallback onSubmit;
   final TrainController enteredWordController;
 
-  Train({Key key, this.entry, this.isCheck, this.onSubmit, this.enteredWordController}) : super(key: key);
+  Train(
+      {Key key,
+      this.entry,
+      this.isCheck,
+      this.onSubmit,
+      this.enteredWordController})
+      : super(key: key);
 
   @override
   _TrainState createState() => _TrainState();
@@ -33,61 +39,53 @@ class _TrainState extends State<Train> {
   Widget build(BuildContext context) {
     final results = widget.isCheck
         ? [
-            Flexible(
-              flex: 1,
-              child: _TrainResult(
-                enteredWord: enteredWord,
-                word: widget.entry.word,
-                isCorrect: widget.enteredWordController.isCorrect,
-              ),
+            // Expanded(
+            //   child:
+            _TrainResult(
+              enteredWord: enteredWord,
+              word: widget.entry.word,
+              isCorrect: widget.enteredWordController.isCorrect,
             ),
+            // ),
           ]
         : [];
     return Form(
       key: _formKey,
-      child: Scrollbar(
-        child: Align(
-          alignment: Alignment.topCenter,
-          child: Column(
-            children: <Widget>[
-              Flexible(
-                flex: 2,
-                child: Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(32.0),
-                    child: Text(widget.entry.translation,
-                        style: Theme.of(context).textTheme.bodyText1),
-                  ),
-                ),
+      child: ListView(
+        children: <Widget>[
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: Text(
+                widget.entry.translation,
+                style: Theme.of(context).textTheme.bodyText1,
+                textAlign: TextAlign.center,
               ),
-              Flexible(
-                flex: 1,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16.0),
-                  child: TextFormField(
-                    textAlign: TextAlign.center,
-                    keyboardType: TextInputType.visiblePassword,
-                    enableSuggestions: false,
-                    enabled: !widget.isCheck,
-                    style: Theme.of(context).textTheme.headline5,
-                    controller: widget.enteredWordController,
-                    decoration: InputDecoration(
-                      filled: true,
-                      hintText: widget.isCheck ? "" : 'Enter a word...',
-                    ),
-                    onFieldSubmitted: (_) {
-                      widget.onSubmit();
-                    },
-                    onChanged: (value) {
-                      enteredWord = value;
-                    },
-                  ),
-                ),
-              ),
-              ...results,
-            ],
+            ),
           ),
-        ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: TextFormField(
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.visiblePassword,
+              enableSuggestions: false,
+              enabled: !widget.isCheck,
+              style: Theme.of(context).textTheme.headline5,
+              controller: widget.enteredWordController,
+              decoration: InputDecoration(
+                filled: true,
+                hintText: widget.isCheck ? "" : 'Enter a word...',
+              ),
+              onFieldSubmitted: (_) {
+                widget.onSubmit();
+              },
+              onChanged: (value) {
+                enteredWord = value;
+              },
+            ),
+          ),
+          ...results,
+        ],
       ),
     );
   }
@@ -103,28 +101,32 @@ class _TrainResult extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => _speak(),
-      child: Card(
-        color: isCorrect ? Colors.green : Colors.red,
+    return Card(
+      color: isCorrect ? Colors.green : Colors.red,
+      child: InkWell(
+        onTap: () => _speak(),
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
               children: [
-                Flexible(
-                    child: Text(isCorrect ? "Correct" : "Wrong",
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodyText1
-                            .copyWith(color: Colors.white))),
-                Flexible(
-                    child: Text(word,
-                        style: Theme.of(context)
-                            .textTheme
-                            .headline4
-                            .copyWith(color: Colors.white))),
+                Text(
+                  isCorrect ? "Correct" : "Wrong",
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyText1
+                      .copyWith(color: Colors.white),
+                ),
+                Text(
+                  word,
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline4
+                      .copyWith(color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
               ],
             ),
           ),
